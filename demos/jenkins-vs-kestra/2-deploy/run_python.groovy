df504995d121b59763afe0537e586e7c3c9fa8f2
Will Russell <wrussell@kestra.io>
2024-11-14 17:04:47 +0000

    Create run_python.groovy

---
+++ demos/jenkins-vs-kestra/2-deploy/run_python.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent { docker { image 'python:3.9.0' } }
+    
+    stages {
+        stage('checkout') {
+            steps {
+                git(
+                    url: 'https://github.com/wrussell1999/kestra-examples.git',
+                    branch: 'main'
+                ) 
+            }
+        }
+        stage('dependencies') {
+            steps {
+                sh 'python3 -m venv .venv'
+            }
+        }
+        stage('tests') {
+            steps {
+                sh '. .venv/bin/activate && python demos/jenkins-vs-kestra/2-deploy/example.py'
+            }
+        }
+    }
+}
