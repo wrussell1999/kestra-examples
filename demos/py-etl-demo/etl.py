0d87b9ccb968c7cd5474e0ad93bbbf3286d13665
Will Russell <wrussell@kestra.io>
2025-01-14 12:34:49 +0000

    Create etl.py

---
+++ demos/py-etl-demo/etl.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+from kestra import Kestra
+
+df = pd.read_csv(os.environ['DATASET_URL'])
+total_revenue = df['total'].sum()
+Kestra.outputs({"total": total_revenue})
+
+discounted_amount = int(os.environ['DISCOUNTED_AMOUNT'])
+if discount_amount > 0:
+  df['discounted_total'] = df['total'] * (1 - discount_amount)
+  df.to_csv(os.environ['FILENAME'])
