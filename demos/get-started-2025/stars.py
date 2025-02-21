3f1fea939a74879f2c83a1f5c175236d8a1e07eb
Will Russell <wrussell@kestra.io>
2025-02-21 16:46:36 +0000

    feat: 2025 getting started example

---
+++ demos/get-started-2025/stars.py
@@ -0,0 +1,6 @@
+import requests
+from kestra import Kestra
+
+r = requests.get('https://api.github.com/repos/kestra-io/kestra')
+gh_stars = r.json()['stargazers_count']
+Kestra.outputs({'gh_stars': gh_stars})
