c4a623a2c61cf8ee580e9c25bc6610e6a9bc8a51
Will Russell <wrussell@kestra.io>
2025-02-25 17:38:53 +0000

    fix: remove indexes

--- demos/py-etl-demo/files/etl.py
+++ demos/py-etl-demo/files/etl.py
@@ -9,4 +9,4 @@ Kestra.outputs({"total": total_revenue})
 discount_amount = float(os.environ['DISCOUNTED_AMOUNT'])
 if discount_amount > 0:
   df['discounted_total'] = df['total'] * (1 - discount_amount)
-  df.to_csv(os.environ['FILENAME'])
+  df.to_csv(os.environ['FILENAME'], index=False)
