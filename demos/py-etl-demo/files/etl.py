756eec553413eee60f1f6be681d3ef040f1ffd6e
Will Russell <wrussell@kestra.io>
2025-05-13 12:31:13 +0100

    Update etl.py

--- demos/py-etl-demo/files/etl.py
+++ demos/py-etl-demo/files/etl.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from kestra import Kestra
 
-df = pd.read_csv(os.environ['DATASET_URL'])
+df = pd.read_csv(os.environ['DATASET'])
 total_revenue = df['total'].sum()
 Kestra.outputs({"total": total_revenue})
 
