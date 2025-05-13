f0a32caab8bba570e726e9aa81c28ad1e9b45575
Will Russell <wrussell@kestra.io>
2025-05-13 12:30:57 +0100

    initial commit for go

---
+++ demos/py-etl-demo/files/etl.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "encoding/csv"
+    "fmt"
+    "os"
+    "strconv"
+)
+
+func main() {
+    dataset := os.Getenv("DATASET_URL")
+    discountStr := os.Getenv("DISCOUNTED_AMOUNT")
+    outFile := os.Getenv("FILENAME")
+
+    discount, _ := strconv.ParseFloat(discountStr, 64)
+
+    f, _ := os.Open(dataset)
+    r := csv.NewReader(f)
+    header, _ := r.Read()
+
+    totalIdx := -1
+    for i, h := range header {
+        if h == "total" {
+            totalIdx = i
+            break
+        }
+    }
+
+    var out [][]string
+    var sum float64
+    if discount > 0 {
+        header = append(header, "discounted_total")
+    }
+    out = append(out, header)
+
+    for {
+        row, err := r.Read()
+        if err != nil {
+            break
+        }
+        val, _ := strconv.ParseFloat(row[totalIdx], 64)
+        sum += val
+        if discount > 0 {
+            disc := val * (1 - discount)
+            row = append(row, fmt.Sprintf("%.2f", disc))
+        }
+        out = append(out, row)
+    }
+
+    output := fmt.Sprintf("::{\"total\": %.2f}::", sum)
+    fmt.Println(output)
+
+    if discount > 0 {
+        w, _ := os.Create(outFile)
+        defer w.Close()
+        csv.NewWriter(w).WriteAll(out)
+    }
+}
